55e402e55f5353d14e51a3cea32a8ee3691efcf4
jinhan <choi.jinhan@gmail.com>
2023-05-02 00:43:57 -0400

    image preview - chrome doesn't support tiff rendering

--- client/src/components/Dragger.js
+++ client/src/components/Dragger.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { message, Upload } from "antd";
+import React, { useState, useContext } from "react";
+import { message, Upload, Modal } from "antd";
 import { InboxOutlined } from "@ant-design/icons";
 import { AppContext } from "../contexts/GlobalContext";
 
@@ -7,6 +7,13 @@ function Dragger() {
   const context = useContext(AppContext);
   const { Dragger } = Upload;
 
+  const getBase64 = (file) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
   const onChange = (info) => {
     const { status } = info.file;
     // if (status !== "uploading") {
@@ -15,14 +22,14 @@ function Dragger() {
     if (status === "done") {
       console.log("done");
       message.success(`${info.file.name} file uploaded successfully.`);
-      context.setFiles(info.fileList);
+      context.setFiles([...info.fileList]);
     } else if (status === "error") {
       console.log("error");
       message.error(`${info.file.name} file upload failed.`);
     } else if (status === "removed") {
       console.log("removed");
       message.alert(`${info.file.name} file removed.`);
-      context.setFiles(info.fileList);
+      context.setFiles([...info.fileList]);
     }
   };
 
@@ -35,31 +42,57 @@ function Dragger() {
     }
   };
 
-  const beforeUpload = (file) => {
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [previewTitle, setPreviewTitle] = useState("");
+
+  const handleCancel = () => setPreviewOpen(false);
+
+  const handlePreview = async (file) => {
     console.log(file);
-    const isTiff = file.type === "image/tiff";
-    if (!isTiff) {
-      message.error(`${file.name} is not a tiff file`);
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
     }
-    return isTiff || Upload.LIST_IGNORE;
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
+    setPreviewTitle(
+      file.name || file.url.substring(file.url.lastIndexOf("/") + 1)
+    );
   };
 
   return (
-    <Dragger
-      name="file"
-      multiple={true}
-      onChange={onChange}
-      customRequest={uploadImage}
-      // beforeUpload={beforeUpload}
-      style={{ maxHeight: "20vh" }}
-    >
-      <p className="ant-upload-drag-icon">
-        <InboxOutlined />
-      </p>
-      <p className="ant-upload-text">
-        Click or drag file to this area to upload
-      </p>
-    </Dragger>
+    <>
+      <Dragger
+        name="file"
+        multiple={true}
+        onChange={onChange}
+        customRequest={uploadImage}
+        onPreview={handlePreview}
+        listType="picture"
+        style={{ maxHeight: "20vh" }}
+      >
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">
+          Click or drag file to this area to upload
+        </p>
+      </Dragger>
+      <Modal
+        open={previewOpen}
+        title={previewTitle}
+        footer={null}
+        onCancel={handleCancel}
+      >
+        <img
+          alt="example"
+          style={{
+            width: "100%",
+          }}
+          src={previewImage}
+        />
+      </Modal>
+    </>
   );
 }
 
