44d7e2b759c157f317bd741cdb7004bfdb907c59
jinhan <choi.jinhan@gmail.com>
2023-06-29 12:06:16 -0400

    change to use Request

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -23,13 +23,13 @@ export async function getNeuroglancerViewer(image, label) {
 
 export async function startModelTraining() {
   try {
-    // let fmData = new FormData();
-    // fmData.append("configBase", "--config-base configs/SNEMI/SNEMI-Base.yaml");
+    let data = JSON.stringify({
+      log_dir: "--log_dir=../../logs/tensorboard/",
+    });
 
-    let fmData = "--config-base configs/SNEMI/SNEMI-Base.yaml";
     const res = await axios.post(
       `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_URL}/start_model_training`,
-      fmData
+      data
     );
     return res.data;
   } catch (error) {
