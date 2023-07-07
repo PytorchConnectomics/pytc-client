496e17c00b35a9014385c485ec03cf5ffd92433e
Luke Sanford <sanforlu@bc.edu>
2023-07-07 15:18:48 -0400

    Small Bug Fixes for starting server and uploading

--- client/src/contexts/GlobalContext.js
+++ client/src/contexts/GlobalContext.js
@@ -7,6 +7,7 @@ export const ContextWrapper = (props) => {
   const [fileList, setFileList] = useState([]);
   const [currentImage, setCurrentImage] = useState(null);
   const [currentLabel, setCurrentLabel] = useState(null);
+  const [trainingConfig, setTrainingConfig] = useState(null);
   return (
     <AppContext.Provider
       value={{
@@ -18,6 +19,8 @@ export const ContextWrapper = (props) => {
         setCurrentImage,
         currentLabel,
         setCurrentLabel,
+        trainingConfig,
+        setTrainingConfig
       }}
     >
       {props.children}
