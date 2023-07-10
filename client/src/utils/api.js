04ce789cc2a30b83c36e12afbf29a7914fe2a941
teddy-ariel <ariele@bc.edu>
2023-07-10 14:15:14 -0400

    Displays error to user when data path entered is not valid

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {message} from "antd";
 export async function getNeuroglancerViewer(
   image,
   label,
@@ -17,11 +18,13 @@ export async function getNeuroglancerViewer(
     );
     return res.data;
   } catch (error) {
-    if (error.response) {
-      throw new Error(
+    message.error(`Invalid Data Path(s). Be sure to include all "/" and that data path is correct.`);
+    /*if (error.response) {
+      /!*throw new Error(
         `${error.response.status}: ${error.response.data?.detail?.error}`
-      );
+      );*!/
+
     }
-    throw error;
+    throw error;*/
   }
 }
