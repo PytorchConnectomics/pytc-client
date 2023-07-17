a9443842d86db80f1dc5b65575b9bc3ce8131ceb
Luke Sanford <sanforlu@bc.edu>
2023-07-17 16:51:27 -0400

    Finalized Advanced Configuration

--- client/src/components/YamlFileEditor.js
+++ client/src/components/YamlFileEditor.js
@@ -3,17 +3,31 @@ import { Upload, Button, message, Input, Slider, Col, Row, InputNumber } from "a
 import { UploadOutlined } from "@ant-design/icons";
 import yaml from "js-yaml";
 import { AppContext } from "../contexts/GlobalContext";
+import { YamlContext } from "../contexts/YamlContext";
 
 {/* WORKING ON MAKING TEXT CHANGES STICK*/}
 const YamlFileEditor = () => {
     const context = useContext(AppContext);
+    const YAMLContext = useContext(YamlContext);
     const [yamlContent, setYamlContent] = useState("");
 
+    const updateYamlData = (property, value) => {
+      const updatedYamlData = { ...context.yamlData, [property]: value };
+      const updatedYamlContent = yaml.safeDump(updatedYamlData, { indent: 2 }).replace(/^\s*\n/gm, "");
+      setYamlContent(updatedYamlContent);
+      context.setTrainingConfig(updatedYamlContent);
+    };
+  
     const handleTextAreaChange = (event) => {
         const updatedYamlContent = event.target.value;
         setYamlContent(updatedYamlContent);
+        context.setTrainingConfig(updatedYamlContent);
         try {
-          const updatedYamlData = yaml.safeLoad(updatedYamlContent);
+          const yamlData = yaml.safeLoad(updatedYamlContent);
+          YAMLContext.setNumGPUs(yamlData.SYSTEM.NUM_GPUS);
+          YAMLContext.setNumCPUs(yamlData.SYSTEM.NUM_CPUS);
+          YAMLContext.setLearningRate(yamlData.SOLVER.BASE_LR);
+          YAMLContext.setSamplesPerBatch(yamlData.SOLVER.SAMPLES_PER_BATCH)
           // Update specific YAML values based on the updated YAML data
           // Update other YAML values as needed
         } catch (error) {
