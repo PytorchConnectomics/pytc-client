83e5475eb7121cba124121d30b3b297951e22b28
jinhan <choi.jinhan@gmail.com>
2023-08-16 11:38:31 -0400

    set default image for tiff format file & set input filed for folder path of file

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,15 +3,15 @@ import { message } from "antd";
 
 export async function getNeuroglancerViewer(
   image,
-  label,
-  image_path,
-  label_path
+  label
+  // image_path,
+  // label_path
 ) {
   // console.log(image, label, image_path, label_path);
   try {
     let data = JSON.stringify({
-      image: image_path + image.name,
-      label: label_path + label.name,
+      image: image.folderPath + image.name,
+      label: label.folderPath + label.name,
     });
     const res = await axios.post(
       `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_URL}/neuroglancer`,
