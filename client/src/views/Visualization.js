1d424863f6f053769da21bf5d1aa57f38b9678ee
jinhan <choi.jinhan@gmail.com>
2023-08-22 12:52:22 -0400

    create ng viewer with random port number TODO: open visualization in a new tab

--- client/src/views/Visualization.js
+++ client/src/views/Visualization.js
@@ -1,19 +1,20 @@
 import React, { useContext } from "react";
 import { AppContext } from "../contexts/GlobalContext";
 
-function Visualization() {
+function Visualization(props) {
   const context = useContext(AppContext);
+  const { viewer } = props;
 
   return (
     <div>
       {"Neuroglancer Viewer"}
-      {context.viewer && (
+      {viewer && (
         <iframe
           width="100%"
           height="800"
           frameBorder="0"
           scrolling="no"
-          src={context.viewer}
+          src={viewer}
         />
       )}
     </div>
