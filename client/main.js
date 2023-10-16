9230c02cca8eef5f45fa6bfbae2dea37bc228f13
Xiangan He <xiangan9@gmail.com>
2023-10-16 10:48:08 -0400

    basic electron app bootup

---
+++ client/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  mainWindow.loadURL(`./build/index.html`);
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
