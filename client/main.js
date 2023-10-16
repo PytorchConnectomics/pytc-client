c4c3a7911b013bd315ad4a7d088e896e6f29ea2d
Xiangan He <xiangan9@gmail.com>
2023-10-16 12:02:42 -0400

    electron app

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const url = require('url');
 const { app, BrowserWindow } = require('electron');
 
 let mainWindow;
@@ -12,7 +14,13 @@ function createWindow() {
     }
   });
 
-  mainWindow.loadURL(`./build/index.html`);
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, './build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  mainWindow.webContents.openDevTools();
+  
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
