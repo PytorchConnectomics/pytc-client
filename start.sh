cadcfd8302b662f05afbaf79f6aedce5d98ee671
xBalbinus <xiangan9@gmail.com>
2024-02-16 09:39:42 -0500

    start.sh improvements

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Get pytorch_connectomics
+git submodule update --init --recursive
+
+# Create a venv
+python3 -m venv .venv
+source .venv/bin/activate
+
+# Install the required packages in /server_api/requirements.txt
+pip install -r server_api/requirements.txt
+
 # Start the API server
 python3 server_api/main.py &
 
