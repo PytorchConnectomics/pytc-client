4994a5ec79aed884a6640edbed2483a590be0d57
mansouralawi <mansour.alawi@gmail.com>
2024-06-13 20:44:22 +0800

    && to introduces error checking

--- start.bat
+++ start.bat
@@ -4,7 +4,7 @@ REM Install dependencies in ./server_api
 pip install -r server_api\requirements.txt
 
 REM Start the API server
-start cmd /C "python server_api\main.py & pause"
+start cmd /C "python server_api\main.py && pause"
 
 REM Start the Pytc-connectomics server
-start cmd /C "python server_pytc\main.py & pause"
+start cmd /C "python server_pytc\main.py && pause"
