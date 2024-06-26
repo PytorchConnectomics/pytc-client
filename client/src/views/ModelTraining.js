ab9e7f8a2a10bc02792e1f27cbbfa95fb504658b
Shen Wang <sam4coding@yahoo.com>
2024-06-25 21:27:03 -0400

    fixed unused variables, missing dependency

--- client/src/views/ModelTraining.js
+++ client/src/views/ModelTraining.js
@@ -1,13 +1,14 @@
-import React, { useContext, useState } from "react";
-import { Button, Space } from "antd";
-import { startModelTraining, stopModelTraining } from "../utils/api";
-import Configurator from "../components/Configurator";
-import { AppContext } from "../contexts/GlobalContext";
+/* global localStorage */
+import React, { useContext, useState } from 'react'
+import { Button, Space } from 'antd'
+import { startModelTraining, stopModelTraining } from '../utils/api'
+import Configurator from '../components/Configurator'
+import { AppContext } from '../contexts/GlobalContext'
 
-function ModelTraining() {
-  const context = useContext(AppContext);
-  const [isTraining, setIsTraining] = useState(false);
-  const [trainingStatus, setTrainingStatus] = useState("");
+function ModelTraining () {
+  const context = useContext(AppContext)
+  const [isTraining, setIsTraining] = useState(false)
+  const [trainingStatus, setTrainingStatus] = useState('')
   // const [tensorboardURL, setTensorboardURL] = useState(null);
   const handleStartButton = async () => {
     try {
@@ -16,40 +17,40 @@ function ModelTraining() {
       //   "configBase",
       //   "--config-base configs/SNEMI/SNEMI-Base.yaml"
       // );
-      console.log(context.uploadedYamlFile);
-      const trainingConfig = localStorage.getItem("trainingConfig");
-      console.log(trainingConfig);
+      console.log(context.uploadedYamlFile)
+      const trainingConfig = localStorage.getItem('trainingConfig')
+      console.log(trainingConfig)
       const res = await startModelTraining(
         context.uploadedYamlFile.name,
         trainingConfig,
         context.outputPath,
         context.logPath
-      );
-      console.log(res);
-      setIsTraining(true);
-      setTrainingStatus("Training in Progress... Please wait, this may take a while.");
+      )
+      console.log(res)
+      setIsTraining(true)
+      setTrainingStatus('Training in Progress... Please wait, this may take a while.')
     } catch (e) {
-      console.log(e);
-      setTrainingStatus("Training error! Please inspect console.");
-      setIsTraining(false);
-      return;
+      console.log(e)
+      setTrainingStatus('Training error! Please inspect console.')
+      setIsTraining(false)
+      return
     }
 
-    setIsTraining(false);
-    setTrainingStatus("Training complete!");
-  };
+    setIsTraining(false)
+    setTrainingStatus('Training complete!')
+  }
 
   const handleStopButton = async () => {
     try {
-      stopModelTraining();
+      stopModelTraining()
     } catch (e) {
-      console.log(e);
-      setTrainingStatus("Training error! Please inspect console.");
+      console.log(e)
+      setTrainingStatus('Training error! Please inspect console.')
     } finally {
-      setIsTraining(false);
-      setTrainingStatus("Training stopped.");
+      setIsTraining(false)
+      setTrainingStatus('Training stopped.')
     }
-  };
+  }
 
   // const handleTensorboardButton = async () => {
   //   try {
@@ -68,8 +69,8 @@ function ModelTraining() {
   return (
     <>
       <div>
-        {/*{"ModelTraining"}*/}
-        <Configurator fileList={context.files} type="training" />
+        {/* {"ModelTraining"} */}
+        <Configurator fileList={context.files} type='training' />
         <Space wrap style={{ marginTop: 12 }}>
           <Button
             onClick={handleStartButton}
@@ -84,11 +85,11 @@ function ModelTraining() {
             Stop Training
           </Button>
         </Space>
-        {/*<Button onClick={handleTensorboardButton}>Tensorboard</Button>*/}
+        {/* <Button onClick={handleTensorboardButton}>Tensorboard</Button> */}
         <p style={{ marginTop: 4 }}>{trainingStatus}</p>
       </div>
     </>
-  );
+  )
 }
 
-export default ModelTraining;
+export default ModelTraining
