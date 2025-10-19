a6bbce96ca748b1946dfc1eae7dbdbc70737686d
Adam Gohain <adamgohain@gmail.com>
2025-10-19 16:45:04 -0400

    clear file cache on startup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,44 @@
+import { useEffect, useState } from 'react'
+import localforage from 'localforage'
 import './App.css'
 import Views from './views/Views'
 import { ContextWrapper } from './contexts/GlobalContext'
 import { YamlContextWrapper } from './contexts/YamlContext'
 
+const FILE_CACHE_KEYS = [
+  'files',
+  'fileList',
+  'imageFileList',
+  'labelFileList',
+  'currentImage',
+  'currentLabel',
+  'inputImage',
+  'inputLabel'
+]
+
 function App () {
+  const [isCacheCleared, setIsCacheCleared] = useState(false)
+
+  useEffect(() => {
+    const clearFileCache = async () => {
+      try {
+        await Promise.all(
+          FILE_CACHE_KEYS.map((key) => localforage.removeItem(key))
+        )
+      } catch (error) {
+        console.error('Failed to clear file cache on startup:', error)
+      } finally {
+        setIsCacheCleared(true)
+      }
+    }
+
+    clearFileCache()
+  }, [])
+
+  if (!isCacheCleared) {
+    return null
+  }
+
   return (
     <ContextWrapper>
       <YamlContextWrapper>
